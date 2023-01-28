0f1fcc63660f5f829e23098fc8576f6bb45103f3
Carlos <carlos.wei.hk@gmail.com>
2023-01-28 18:09:53 +0800

    [rust] add utils
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>

---
+++ rust_programming/array/src/main.rs
@@ -0,0 +1,6 @@
+include!("../../utils/src/utils.rs");
+
+fn main() {
+    let max = utils_max(1, 5);
+    println!("Hello, world! max is {:?}", max);
+}
---
+++ rust_programming/utils/src/utils.rs
@@ -0,0 +1,7 @@
+fn utils_max(n:u64, m:u64) -> u64 {
+    if n > m {
+        return n;
+    } else {
+        return m;
+    }
+}
