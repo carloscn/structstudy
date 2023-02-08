c8aa9df1cc649e642465017803514194b6530a25
Carlos <carlos.wei.hk@gmail.com>
2023-02-08 09:17:42 +0800

    [leetcode-657] rust version
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>

---
+++ rust_programming/str/src/36_robot-return-to-origin_657.rs
@@ -0,0 +1,33 @@
+extern crate utils;
+
+fn is_move_back(vector: &str) -> bool
+{
+    let mut pos = (0_i32 ,0_i32);
+
+    if vector.len() == 0 {
+        return true;
+    }
+
+    for e in vector.chars() {
+        match e {
+            'l' | 'L' => pos.0 += 1,
+            'r' | 'R' => pos.0 -= 1,
+            'u' | 'U' => pos.1 += 1,
+            'd' | 'D' => pos.1 -= 1,
+            _ => return false
+        }
+    }
+
+    if pos == (0 ,0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn test_is_move_back()
+{
+    let vector = "LRUDD".to_string();
+    let ret = is_move_back(&vector);
+    println!("the return is {ret}");
+}
--- rust_programming/str/src/main.rs
+++ rust_programming/str/src/main.rs
@@ -1,8 +1,9 @@
 //include!("34_longest-uncommon-subsequence-i_521.rs");
-include!("35_reverse-string-ii_541.rs");
-
+//include!("35_reverse-string-ii_541.rs");
+include!("36_robot-return-to-origin_657.rs");
 fn main() {
-    test_reverse_string_ii();
+    //test_reverse_string_ii();
+    test_is_move_back();
     //test_longest_uncommon_subseq();
     println!("array test vector done!");
 }
