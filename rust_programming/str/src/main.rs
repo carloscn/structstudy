2477680514911e8724afaa94eb3bd2652f3b5fc6
Carlos <carlos.wei.hk@gmail.com>
2023-02-11 08:22:01 +0800

    [leetcode-680] rust version
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>

--- rust_programming/str/src/main.rs
+++ rust_programming/str/src/main.rs
@@ -1,12 +1,14 @@
 mod n36_robot_return_to_origin_657;
 mod n35_reverse_tring_ii_541;
 mod n34_longest_uncommon_subsequence_i_521;
+mod n37_valid_palindrome_ii_680;
 
 fn main() {
 
     n36_robot_return_to_origin_657::test_is_move_back();
     n35_reverse_tring_ii_541::test_reverse_string_ii();
     n34_longest_uncommon_subsequence_i_521::test_longest_uncommon_subseq();
+    n37_valid_palindrome_ii_680::test_valid_palindrome_ii();
 
     println!("array test vector done!");
 }
---
+++ rust_programming/str/src/n37_valid_palindrome_ii_680.rs
@@ -0,0 +1,47 @@
+
+fn valid_palindrome_ii(in_str:&str) -> bool
+{
+    let len:usize = in_str.len();
+    let mut tol:bool = false;
+    let binding = String::from(in_str);
+    let char_list = binding.as_bytes();
+    let mut i:usize = 0;
+    let mut j:usize = len - 1;
+
+    while i != j {
+        if char_list[i] != char_list[j] {
+            if tol == false {
+                if char_list[i + 1] == char_list[i] {
+                    i += 1;
+                    tol = true;
+                    continue;
+                } else if char_list[i] == char_list[j - 1] {
+                    j -= 1;
+                    tol = true;
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+
+        }
+        i += 1;
+        j -= 1;
+        if (j - i) == 1 {
+            break;
+        }
+    }
+
+    return true;
+}
+
+
+pub fn test_valid_palindrome_ii()
+{
+    let mut res:bool = false;
+
+    res = valid_palindrome_ii("abcddcbqa");
+    println!("thre result is {res}");
+}
