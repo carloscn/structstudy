508158635cc311da0adf429e20107475a38a672c
Haochen <carlos.wei.hk@gmail.com>
2023-03-08 18:03:42 +0800

    [leetcode-43] rust version
    Signed-off-by: Haochen <carlos.wei.hk@gmail.com>
    Change-Id: Ic054c465bab6f61fbaa30c40a0b9279b5f9781c2

--- rust_programming/str/src/main.rs
+++ rust_programming/str/src/main.rs
@@ -10,6 +10,7 @@ mod n42_jewels_and_stones_771;
 mod n43_rotate_string_796;
 mod n44_most_common_word_819;
 mod n45_find_the_index_of_the_first_occurrence_in_a_string_28;
+mod n46_multiply_strings_43;
 fn main() {
     println!("hello leetcode string project!");
 }
