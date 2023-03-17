9f2d1acade893b9662d0a22b13c7dfbab1ea99dd
Haochen <carlos.wei.hk@gmail.com>
2023-03-17 13:09:26 +0800

    [utils] add utils_memmove.c
    Signed-off-by: Haochen <carlos.wei.hk@gmail.com>
    Change-Id: I6ceb02408ccf297d547eca1a21d6a2f78d8ff61e

--- c_programming/utils/utils.c
+++ c_programming/utils/utils.c
@@ -792,6 +792,46 @@ finish:
     return ret;
 }
 
+int32_t utils_memmove(void *dest, const void *src, size_t n)
+{
+    uint8_t *from = (uint8_t *) src;
+    uint8_t *to = (uint8_t *) dest;
+
+    if (from == to || 0 == n) {
+        return dest;
+    }
+
+    if (to > from && to - from < (int)n) {
+    	/* to overlaps with from */
+		/*  <from......>         */
+		/*         <to........>  */
+		/* copy in reverse, to avoid overwriting from */
+        int32_t i;
+        for (i = n - 1; i >= 0; i --) {
+            to[i] = from[i];
+        }
+
+        goto finish;
+    }
+
+    if (form > to && from - to < (int)n) {
+		/* to overlaps with from */
+		/*        <from......>   */
+		/*  <to........>         */
+		/* copy forwards, to avoid overwriting from */
+        size_t i;
+        for (i = 0; i < n; i ++) {
+            to[i] = from[i];
+        }
+
+        goto finish;
+    }
+
+    memcpy(dest, src, n);
+finish:
+    return dest;
+}
+
 #ifdef __cplusplus
 }
 #endif /*__cplusplus*/
