1a862763f2618fdcb9c99d2fcdf7b48b4cbd65a6
Carlos <carlos.wei.hk@gmail.com>
2023-04-09 10:32:57 +0800

    [leetcode-56] merge intervals
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>
    Change-Id: Idbdfe58d43c06551b8acf8feccdf45db954c64ae

--- c_programming/utils/bufferlist.h
+++ c_programming/utils/bufferlist.h
@@ -17,6 +17,7 @@ int32_t buflist_insert_after(BUFLIST_T *buflist, BUFFER_T *buf, size_t index);
 int32_t buflist_remove_last(BUFLIST_T *buflist);
 int32_t buflist_remove_at(BUFLIST_T *buflist, size_t index);
 BUFFER_T* buflist_get_buffer_at(BUFLIST_T *buflist, size_t index);
+BUFFER_T* buflist_get_buffer_dup_at(BUFLIST_T *buflist, size_t index);
 bool buflist_contains(BUFLIST_T *buflist, BUFFER_T* buf, size_t *index);
 size_t buflist_get_size(BUFLIST_T *buflist);
 int32_t buflist_append_all_buflist(BUFLIST_T *buflist, BUFLIST_T *buflist_app);
