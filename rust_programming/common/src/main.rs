18eac460f57c1a62f2d84794892898fa7622c2d4
Carlos <carlos.wei.hk@gmail.com>
2023-04-23 08:24:51 +0800

    [leetcode-1232] check_if_it_is_a_straight_line
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>
    Change-Id: Ib5c1a1ca858f518ba6c411fb28c6810233e400ed

--- rust_programming/common/src/main.rs
+++ rust_programming/common/src/main.rs
@@ -1,5 +1,6 @@
 mod n18_binary_number_with_alternating_bits_693;
 mod n19_smallest_range_i_908;
+mod n20_check_if_it_is_a_straight_line_1232;
 fn main() {
     n18_binary_number_with_alternating_bits_693::test_has_alternating_bits();
     println!("common list executed!");
