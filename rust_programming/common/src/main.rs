39277eff1871ec3619d310ef2cbdd657242ffacc
Carlos Wei <carlos.wei.hk@gmail.com>
2023-04-26 08:37:06 +0800

    [leetcode-1281] subtract-the-product-and-sum-of-digits-of-an-integer
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>
    Change-Id: Ie78c1ab239c1ceb77a0d8e28aadd3055e1f3979c

--- rust_programming/common/src/main.rs
+++ rust_programming/common/src/main.rs
@@ -1,6 +1,7 @@
 mod n18_binary_number_with_alternating_bits_693;
 mod n19_smallest_range_i_908;
 mod n20_check_if_it_is_a_straight_line_1232;
+mod n21_subtract_the_product_and_sum_of_digits_of_an_integer_1281;
 fn main() {
     n18_binary_number_with_alternating_bits_693::test_has_alternating_bits();
     println!("common list executed!");
