72e728007f15b8ffba271c426a0db7b777341e18
Carlos <carlos.wei.hk@gmail.com>
2023-06-03 08:59:22 +0800

    [leetcode-89] gray code
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>
    Change-Id: Ie4c2c3e4ca8ee41ffdae09473d2aa8624bb6f9e4

---
+++ c_programming/array/n54_gray_code_89.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "utils.h"
+
+int32_t gray_code(size_t n, int32_t *array, size_t *o_len)
+{
+    int32_t ret = 0;
+
+    UTILS_CHECK_PTR(array);
+    UTILS_CHECK_PTR(o_len);
+
+
+
+finish:
+    return ret;
+}
