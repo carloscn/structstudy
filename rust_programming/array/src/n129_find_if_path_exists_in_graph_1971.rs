47fa86498a0a6f5aca1aabd2b9468edd3f723da7
carlos <carlos.wei.hk@gmail.com>
2023-08-25 14:59:31 +0800

    [leetcode-1971] find if path exists in graph
    Signed-off-by: carlos <carlos.wei.hk@gmail.com>
    Change-Id: Ib8fee984daaf6774c5a2a3b75bc9177b6bc1b5ad

---
+++ rust_programming/array/src/n129_find_if_path_exists_in_graph_1971.rs
@@ -0,0 +1,26 @@
+
+fn is_in_same_path(path:&Vec<i32>, i_source:usize, i_dest:usize) -> bool
+{
+    let mut i:usize = 0;
+
+
+
+
+    return false;
+}
+
+pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool
+{
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn demo() {
+
+    }
+}
