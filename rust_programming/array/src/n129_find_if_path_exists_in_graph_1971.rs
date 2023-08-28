d0d42c22c68638945f8f383f6c8ad4494b4b0d4f
carlos <carlos.wei.hk@gmail.com>
2023-08-28 08:35:34 +0800

    [leetcode-1971] Find if Path Exists in Graph
    Signed-off-by: carlos <carlos.wei.hk@gmail.com>
    Change-Id: Id9179de12fc4ab711f61c73bf577054f566a85ff

--- rust_programming/array/src/n129_find_if_path_exists_in_graph_1971.rs
+++ rust_programming/array/src/n129_find_if_path_exists_in_graph_1971.rs
@@ -1,17 +1,50 @@
 
-fn is_in_same_path(path:&Vec<i32>, i_source:usize, i_dest:usize) -> bool
+fn init(father:&mut Vec<i32>)
 {
-    let mut i:usize = 0;
+    for i in 0..father.len() {
+        father[i] = i as i32;
+    }
+}
 
+fn find(father:&mut Vec<i32>, u:i32) -> i32
+{
+    if u != father[u as usize] {
+        father[u as usize] = find(father, father[u as usize]);
+        return father[u as usize];
+    } else {
+        return u;
+    }
+}
 
+fn join(father:&mut Vec<i32>, u:i32, v:i32)
+{
+    let a = find(father, u);
+    let b = find(father, v);
+    if a == b {
+        return;
+    }
+    father[b as usize] = a;
+}
 
-
-    return false;
+fn same(father:&mut Vec<i32>, u:i32, v:i32) -> bool
+{
+    let a = find(father, u);
+    let b = find(father, v);
+    return a == b;
 }
 
 pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool
 {
+    let mut i:usize = 0;
+    let mut father:Vec<i32> = vec![0;n as usize];
 
+    init(&mut father);
+
+    for i in 0..edges.len() {
+        join(&mut father, edges[i][0], edges[i][1]);
+    }
+
+    return same(&mut father, source, destination);
 }
 
 #[cfg(test)]
@@ -21,6 +54,9 @@ mod tests {
 
     #[test]
     fn demo() {
-
+        let ret = valid_path(3, vec![vec![0,1],vec![1,2],vec![2,0]], 0, 2);
+        assert_eq!(ret, true);
+        let ret = valid_path(6, vec![vec![0,1],vec![0,2],vec![3,5],vec![5,4],vec![4,3]], 0, 5);
+        assert_eq!(ret, false);
     }
 }
