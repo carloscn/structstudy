d9c3ff0284fb4a57a0ac1b5a40ff717cad14e2b0
carlos <carlos.wei.hk@gmail.com>
2023-11-02 08:13:19 +0800

    [leetcode-2506] Count Pairs Of Similar Strings
    Change-Id: If1d39e11f2cc1cea8aa0618e66bc2b20f283a048
    Signed-off-by: carlos <carlos.wei.hk@gmail.com>

--- rust_programming/str/src/main.rs
+++ rust_programming/str/src/main.rs
@@ -98,6 +98,7 @@ mod n130_remove_letter_to_equalize_frequency_2423;
 mod n131_odd_string_difference_2451;
 mod n132_circular_sentence_2490;
 mod n133_maximum_value_of_a_string_in_an_array_2496;
+mod n134_count_pairs_of_similar_strings_2506;
 fn main() {
     println!("hello leetcode string project!");
 }
